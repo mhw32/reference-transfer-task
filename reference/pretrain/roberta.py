40b5b91d5a38a654f25899dc5427146798b357e0
Mike Wu <me@mikewuis.me>
2019-11-27 16:42:17 -0800

    grabbing roberta features

--- reference/pretrain/roberta.py
+++ reference/pretrain/roberta.py
@@ -12,7 +12,6 @@ if __name__ == "__main__":
     def extract(raw_text_list):
         features = []
         for raw_text in raw_text_list:
-            breakpoint()
             raw_text = ' '.join(raw_text)
             tokens = roberta.encode(raw_text)
             # last layer features [1, 5, 1024]
