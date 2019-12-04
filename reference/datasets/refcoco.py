2575f3a2953773e4d2b3cce0d1b388c0e4a0f1c0
mhw32 <me@mikewuis.me>
2019-12-04 12:40:44 -0800

    updates to dataset

--- reference/datasets/refcoco.py
+++ reference/datasets/refcoco.py
@@ -2,6 +2,7 @@ import os
 import sys
 import math
 import json
+import copy
 import pickle
 import numpy as np
 import pandas as pd
@@ -22,9 +23,6 @@ from reference.text_utils import (
     UNK_TOKEN,
 )
 from reference.utils import OrderedCounter
-
-sys.path.append(os.path.dirname(__file__))
-from refer import REFER
 
 
 class RefCOCOInContext(data.Dataset):
@@ -62,53 +60,41 @@ class RefCOCOInContext(data.Dataset):
         self.max_sent_len = max_sent_len
         self.random_seed = random_seed
 
-        data_name = os.path.basename(data_dir)
-        data_dir = os.path.dirname(data_dir)
-        
-        assert split in ['train', 'val', 'test']
-        assert data_name in ['refclef', 'refcoco', 'refcoco+']
-        
-        self.refer = REFER(data_dir, data_name, 'google')
-        self.ref_ids = self.refer.getRefIds(split=self.split)
+        pickle_path = os.path.join(self.data_dir, f'{self.split}.pickle')
+        with open(pickle_path, 'rb') as fp:
+            data_pickle = pickle.load(fp)
+
+        images = data_pickle['img_paths']
+        masks = data_pickle['masks']
+        texts = data_pickle['texts']
+
+        assert len(images) == len(masks)
+        assert len(images) == len(texts)
+        size = len(images)
 
         if data_size is not None:
+            assert split == 'train'
             rs = np.random.RandomState(self.random_seed)
-            n_train_total = len(self.ref_ids)
-            indices = np.arange(n_train_total)
-            n_train_total = int(math.ceil(data_size * n_train_total))
-            indices = rs.choice(indices, size=n_train_total)
-            self.ref_ids = [self.ref_ids[idx] for idx in indices]
-
-        self.img_ids = self.refer.getImgIds(ref_ids=self.ref_ids)
-        self.img_refs = [self.refer.imgToRefs[img_id] for img_id in self.img_ids]
-        self.img_masks = self.get_masks(self.img_refs)
-        self.img_texts = self.get_text(self.img_refs)
-
-        self.flat_img_ids = []
-        self.flat_img_ctxs = []
-        self.flat_img_refs = []
-        self.flat_img_masks = []
-        self.flat_img_texts = []
-
-        for i in range(len(self.img_ids)):
-            img_refs = self.img_refs[i]
-            img_masks = self.img_masks[i]
-            img_texts = self.img_texts[i]
-            img_id = self.img_ids[i]
-
-            assert len(img_refs) == len(img_masks)
-            assert len(img_refs) == len(img_texts)
-
-            img_ctxs = []
-
-            self.flat_img_refs.extend(img_refs)
-            self.flat_img_masks.extend(img_masks)
-            self.flat_img_texts.extend(img_texts)
-            self.flat_img_ids.extend([img_id for _ in range(len(img_refs))])
+            indices = np.arange(size)
+            size = int(math.ceil(data_size * size))
+            indices = rs.choice(indices, size=size)
+            images = [images[idx] for idx in indices]
+            masks = [masks[idx] for idx in indices]
+            texts = [texts[idx] for idx in indices]
 
         if vocab is None:
             print('Building vocabulary')
-            self.vocab = self.build_vocab(self.flat_img_texts)
+            
+            def flatten_texts(img_texts):
+                all_texts = []
+                for i in range(len(img_texts)):
+                    texts = img_texts[i]
+                    texts = [' '.join(text) for text in texts]
+                    all_texts.extend(texts)
+                return all_texts
+
+            flat_texts = flatten_texts(texts)
+            self.vocab = self.build_vocab(flat_texts)
         else:
             self.vocab = vocab
 
@@ -125,33 +111,54 @@ class RefCOCOInContext(data.Dataset):
         self.pad_index = self.w2i[self.pad_token]
         self.unk_index = self.w2i[self.unk_token]
 
-        flat_img_text_seqs, flat_img_text_lens, flat_img_text_raws = \
-            self._process_text(flat_img_texts)
+        # right now, these are stored 
+        #   [img_path, ...]
+        #   [[bunch of masks], ...]
+        #   [[bunch of texts], ...]
+        # 
+        # we want to get these into
+        #   [img_path, ...]
+        #   [[mask_a, mask_b, ...], ...]
+        #   [[text_a, text_b, ...], ...]
+        #   [label, ...]
+        images, masks, texts, labels = self._unroll_data(images, masks, texts)
 
-        self.flat_img_text_seqs = flat_img_text_seqs
-        self.flat_img_text_lens = flat_img_text_lens
-        self.flat_img_text_raws = flat_img_text_raws
+        # convert raw tokens -> vector of vocabulary indices
+        text_seqs, text_lens, text_raws = self._process_text(texts)
 
-    def get_masks(self, imgToRefs):
-        imgToMasks = []
-        for refs in imgToRefs:
-            masks = [self.refer.getMask(ref) for ref in refs]
-            imgToMasks.append(masks)
-        return imgToMasks
+        self.images = images
+        self.masks = masks
+        self.texts = texts
+        self.labels = labels
+        self.text_seqs = text_seqs
+        self.text_lens = text_lens
+        self.text_raws = text_raws
+        self.size = len(images)
 
-    def get_text(self, imgToRefs):
-        ref_sents = []
-        flat_sents = []
-        for refs in imgToRefs:
-            sents = []
-            for ref in refs:
-                # just take the first sentence
-                sent = ref['sentences'][0]
-                sent = ' '.join(sent['tokens'])
-                sents.append(sent)
-                flat_sents.append(sent)
-            ref_sents.append(sents)
-        return ref_sents
+    def _unroll_data(self, images, masks, texts):
+        assert len(images) == len(masks)
+        assert len(images) == len(texts)
+
+        all_images = []
+        all_masks  = []
+        all_texts  = []
+        all_labels = []
+
+        for i in tqdm(range(len(images))):
+            image, mask, text = images[i], masks[i], texts[i]
+            assert len(mask) == len(text)
+            
+            for j in range(len(mask)):
+                mask_j = copy.deepcopy(mask)
+                text_j = text[j]
+                label = j
+
+                all_images.append(image)
+                all_masks.append(mask_j)
+                all_texts.append(text_j)
+                all_labels.append(label)
+
+        return all_images, all_masks, all_texts, all_labels
 
     def build_vocab(self, texts):
         w2i = dict()
@@ -166,7 +173,6 @@ class RefCOCOInContext(data.Dataset):
         pbar = tqdm(total=len(texts))
         for text in texts:
             tokens = text.lower().split(' ')
-            tokens = clean_tokens(tokens)
             w2c.update(tokens)
             pbar.update()
         pbar.close()
@@ -185,13 +191,11 @@ class RefCOCOInContext(data.Dataset):
         text_seq, text_len, raw_tokens = [], [], []
 
         for i in range(len(text)):
-            sents = text[i]
-            sent_seqs = []
-            sent_lens = []
-            sent_tokens = []
+            sent_seqs, sent_lens, sent_tokens = [], [], []
 
+            sents = text[i]
             for sent in sents:
-                _tokens = sent.lower().split(' ')
+                _tokens = [tok.lower() for tok in sent]
                 tokens = [SOS_TOKEN] + _tokens[:self.max_sent_len] + [EOS_TOKEN]
                 length = len(tokens)
                 tokens.extend([PAD_TOKEN] * (self.max_sent_len + 2 - length))
@@ -208,26 +212,25 @@ class RefCOCOInContext(data.Dataset):
         return text_seq, text_len, raw_tokens
 
     def __len__(self):
-        return len(self.flat_img_ids)
+        return self.size
 
     def __gettext__(self, index):
-        return self.flat_img_text_raws[index]
+        return ' '.join(self.text_raws[index])
 
     def __getitem__(self, index):
-        img_id = self.img_ids[index]
-        image = self.refer.Imgs[img_id]
-        image = Image.open(os.path.join(self.data_dir, image['file_name']))
-        
+        image = self.images[index]
+        masks = self.masks[index]
+        label = self.labels[index]
+        text_seq = self.text_seqs[index]
+        text_len = self.text_lens[index]
+
+        image = Image.open(os.path.join(self.data_dir, image))
+
         if self.image_transform is None:
             image = transforms.ToTensor()(image)
         else:
             image = self.image_transform(image)
 
-        mask = self.flat_img_masks[img_id]
-        text_seqs = self.flat_img_text_seqs[img_id]
-        text_lens = self.flat_img_text_lens[img_id]
-        text_raws = self.flat_img_text_raws[img_id]
+        text_seq = torch.from_numpy(np.array(text_seq)).long()
 
-        text_seq = torch.from_numpy(np.array(text_seqs)).long()
-
-        return index, image, mask, text_seq, text_len
+        return index, image, mask, text_seq, text_len, label
