0837f62effd43965036174fdadf127ffff770027
mhw32 <me@mikewuis.me>
2019-12-19 09:30:41 -0800

    make hugging face so i can run them all

--- reference/pretrain/huggingface.py
+++ reference/pretrain/huggingface.py
@@ -33,54 +33,67 @@ if __name__ == "__main__":
                         choices=['all', 'far', 'close'])
     parser.add_argument('--split-mode', type=str, default='easy',
                         choices=['easy', 'hard'])
+    parser.add_argument('--all', action='store_true', default=False)
     parser.add_argument('--batch-size', type=int, default=128)
     parser.add_argument('--gpu-device', type=int, default=0)
     parser.add_argument('--cuda', action='store_true', default=False)
     parser.add_argument('--seed', type=int, default=42)
     args = parser.parse_args()
 
-    model_class, tokenizer_class, pretrained_weights = MODELS[args.nlp_model]
-    # load pretrained model/tokenizer
-    tokenizer = tokenizer_class.from_pretrained(pretrained_weights)
-    model = model_class.from_pretrained(pretrained_weights)
+    def run_model(nlp_model):
+        model_class, tokenizer_class, pretrained_weights = MODELS[nlp_model]
+        # load pretrained model/tokenizer
+        tokenizer = tokenizer_class.from_pretrained(pretrained_weights)
+        model = model_class.from_pretrained(pretrained_weights)
 
-    def extract(raw_text_list):
-        features = []
-        for raw_text in raw_text_list:
-            raw_text = ' '.join(raw_text)
-            
-            # Add special tokens takes care of adding [CLS], [SEP], <s>... tokens in the right way for each model.
-            input_ids = torch.tensor([tokenizer.encode(raw_text, add_special_tokens=True)])
-            with torch.no_grad():
-                last_hidden_states = model(input_ids)[0]
-                # take a mean over the words
-                sent_features = last_hidden_states.mean(1)
-            features.append(sent_features.detach().cpu())
-        features = torch.cat(features, dim=0)
-        return features
+        def extract(raw_text_list):
+            features = []
+            for raw_text in raw_text_list:
+                raw_text = ' '.join(raw_text)
+                
+                # Add special tokens takes care of adding [CLS], [SEP], <s>... tokens in the right way for each model.
+                input_ids = torch.tensor([tokenizer.encode(raw_text, add_special_tokens=True)])
+                with torch.no_grad():
+                    last_hidden_states = model(input_ids)[0]
+                    # take a mean over the words
+                    sent_features = last_hidden_states.mean(1)
+                features.append(sent_features.detach().cpu())
+            features = torch.cat(features, dim=0)
+            return features
 
-    agent = FeatureAgent(
-        args.dataset,
-        args.data_dir,
-        context_condition = args.context_condition,
-        split_mode = args.split_mode,
-        image_size = 64,
-        override_vocab = None, 
-        batch_size = args.batch_size,
-        gpu_device = args.gpu_device, 
-        cuda = args.cuda,
-        seed = args.seed,
-        image_transforms = None,
-    )
+        agent = FeatureAgent(
+            args.dataset,
+            args.data_dir,
+            context_condition = args.context_condition,
+            split_mode = args.split_mode,
+            image_size = 64,
+            override_vocab = None, 
+            batch_size = args.batch_size,
+            gpu_device = args.gpu_device, 
+            cuda = args.cuda,
+            seed = args.seed,
+            image_transforms = None,
+        )
 
-    train_text_embs = agent.extract_features(extract, modality='text', split='train')
-    val_text_embs = agent.extract_features(extract, modality='text', split='val')
-    test_text_embs = agent.extract_features(extract, modality='text', split='test')
+        train_text_embs = agent.extract_features(extract, modality='text', split='train')
+        val_text_embs = agent.extract_features(extract, modality='text', split='val')
+        test_text_embs = agent.extract_features(extract, modality='text', split='test')
 
-    out_dirname = f'/mnt/fs5/wumike/reference/pretrain/{args.dataset}/huggingface/{args.nlp_model}'
-    if not os.path.isdir(out_dirname):
-        os.makedirs(out_dirname)
-    
-    np.save(f'{out_dirname}/train.npy', train_text_embs.numpy())
-    np.save(f'{out_dirname}/val.npy', val_text_embs.numpy())
-    np.save(f'{out_dirname}/test.npy', test_text_embs.numpy())
+        out_dirname = f'/mnt/fs5/wumike/reference/pretrain/{args.dataset}/huggingface/{nlp_model}'
+        if not os.path.isdir(out_dirname):
+            os.makedirs(out_dirname)
+        
+        np.save(f'{out_dirname}/train.npy', train_text_embs.numpy())
+        np.save(f'{out_dirname}/val.npy', val_text_embs.numpy())
+        np.save(f'{out_dirname}/test.npy', test_text_embs.numpy())
+
+    if args.all: 
+        nlp_models = [
+            'bert', 'gpt_openai', 'gpt_2', 'ctrl', 'transforxl', 
+            'xlnet', 'xlm', 'distilbert', 'roberta',
+        ]
+    else:
+        nlp_models = [args.nlp_model]
+
+    for nlp_model in nlp_models:
+        run_model(nlp_model)
